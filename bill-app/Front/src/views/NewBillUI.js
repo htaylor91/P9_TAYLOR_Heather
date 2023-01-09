326628aa6e44196207734d9ef05b9725248fac2b
htaylor91 <htaylor91@gmail.com>
2023-01-09 17:26:26 +0100

    fix(NewBillUI): 🐛 update form input attributes
    add name attributes to form inputs, add missing data-testid attributes for testing purposes, add required attribute to required form inputs, add aria-errormessage attribute and corresponding span elements for expense-name and file, add aria-invalid="false" attribute to expense-name and file, add accept=".jpg, .jpeg, .png" to file input for client side file extension validation
    
    #4

--- bill-app/Front/src/views/NewBillUI.js
+++ bill-app/Front/src/views/NewBillUI.js
@@ -1,5 +1,8 @@
 import VerticalLayout from './VerticalLayout.js'
 
+/**
+ * Bug Hunt - Bills
+ */
 export default () => {
 
   return (`
@@ -15,7 +18,7 @@ export default () => {
                 <div class="col-md-6">
                   <div class="col-half">
                     <label for="expense-type" class="bold-label">Type de dépense</label>
-                      <select required class="form-control blue-border" data-testid="expense-type">
+                      <select required class="form-control blue-border" name="expense-type" id="expenseType" data-testid="expense-type">
                         <option>Transports</option>
                         <option>Restaurants et bars</option>
                         <option>Hôtel et logement</option>
@@ -27,42 +30,46 @@ export default () => {
                   </div>
                   <div class="col-half">
                     <label for="expense-name" class="bold-label">Nom de la dépense</label>
-                    <input type="text" class="form-control blue-border" data-testid="expense-name" placeholder="Vol Paris Londres" />
+                    <input required type="text" class="form-control blue-border" id="expense-name" name="expenseName" data-testid="expense-name" placeholder="Vol Paris Londres" 
+                    aria-invalid="false" aria-errormessage="errName"/>
+                    <span id="errName" class="errormessage">Veuillez saisir le nom de la dépense</span>
                   </div>
                   <div class="col-half">
                     <label for="datepicker" class="bold-label">Date</label>
-                    <input required type="date" class="form-control blue-border" data-testid="datepicker" />
+                    <input required type="date" class="form-control blue-border" name="datepicker" id="datepicker" data-testid="datepicker" />
                   </div>
                   <div class="col-half">
                     <label for="amount" class="bold-label">Montant TTC </label>
-                    <input required type="number" class="form-control blue-border input-icon input-icon-right" data-testid="amount" placeholder="348"/>
+                    <input required type="number" class="form-control blue-border input-icon input-icon-right" name="amount" id="amount" data-testid="amount" min="1" placeholder="348"/>
                   </div>
                   <div class="col-half-row">
-                    <div class="flex-col"> 
+                    <div class="flex-col">
                       <label for="vat" class="bold-label">TVA</label>
-                      <input type="number" class="form-control blue-border" data-testid="vat" placeholder="70" />
+                      <input required type="number" class="form-control blue-border" name="vat" id="vat" data-testid="vat" min="0" placeholder="70" />
                     </div>
                     <div class="flex-col">
                       <label for="pct" class="white-text">%</label>
-                      <input required type="number" class="form-control blue-border" data-testid="pct" placeholder="20" />
+                      <input required type="number" class="form-control blue-border" name="pct" id="pct" data-testid="pct" min="0" placeholder="20" />
                     </div>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="col-half">
                     <label for="commentary" class="bold-label">Commentaire</label>
-                    <textarea class="form-control blue-border" data-testid="commentary" rows="3"></textarea>
+                    <textarea name="commentary" class="form-control blue-border" id="commentary" data-testid="commentary" rows="3"></textarea>
                   </div>
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
-                    <input required type="file" class="form-control blue-border" data-testid="file" />
+                    <input required name="file" type="file" class="form-control blue-border" id="file" data-testid="file" accept=".jpg, .jpeg, .png"
+                    aria-errormessage="errFile" aria-invalid="false"/>
+                    <span id="errFile" data-testid="errFile" class="errormessage">Veuillez télécharger une image en format .jpg .jpeg ou .png</span>
                   </div>
                 </div>
             </div>
             <div class="row">
               <div class="col-md-6">
                 <div class="col-half">
-                  <button type="submit" id='btn-send-bill' class="btn btn-primary">Envoyer</button>
+                  <button type="submit" id='btn-send-bill' data-testid="btn-send-bill" class="btn btn-primary">Envoyer</button>
                 </div>
               </div>
             </div>
