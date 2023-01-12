5422a9286c1cff8c2cf821da536ec3dbce69a610
htaylor91 <htaylor91@gmail.com>
2023-01-12 14:45:38 +0100

    fix(LoginUI): 🦺 update email and password input ids to resolve duplicate id W3C validation error

--- bill-app/Front/src/views/LoginUI.js
+++ bill-app/Front/src/views/LoginUI.js
@@ -18,10 +18,10 @@ export default () => {
               <div class="card-body">
                 <form class="form-signin" data-testid="form-employee">
                   <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
-                  <label for="inputEmail">Votre email</label>
-                  <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
-                  <label for="inputPassword">Mot de passe</label>
-                  <input type="password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
+                  <label for="employeeEmail">Votre email</label>
+                  <input type="email" id="employeeEmail" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                  <label for="employeePassword">Mot de passe</label>
+                  <input type="password" id="employeePassword" data-testid="employee-password-input" class="form-control" placeholder="******" required>
                   <button class="btn btn-lg btn-primary btn-block" data-testid="employee-login-button" style="background-color: #0E5AE5;" type="submit">Se connecter</button>
                 </form>
               </div>
@@ -32,10 +32,10 @@ export default () => {
               <div class="card-body">
                 <form class="form-signin" data-testid="form-admin">
                   <h2 class="h3 mb-3 font-weight-normal">Administration</h1>
-                  <label for="inputEmail">Votre email</label>
-                  <input type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
-                  <label for="inputPassword">Mot de passe</label>
-                  <input type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
+                  <label for="adminEmail">Votre email</label>
+                  <input type="email" id="adminEmail" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required>
+                  <label for="adminPassword">Mot de passe</label>
+                  <input type="password" id="adminPassword" data-testid="admin-password-input" class="form-control" placeholder="******" required>
                   <button  type="submit" class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;">Se connecter</button>
                 </form>
               </div>
